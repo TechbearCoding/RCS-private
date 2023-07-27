aadbc31ca66bb759d82120953d1f70d68f08132d
m4 <martins@techbearcoding.lv>
2023-07-27 19:12:40 +0300

    More samples

--- day2_if/Program.cs
+++ day2_if/Program.cs
@@ -6,13 +6,29 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+            /*Console.WriteLine(SaskaitisanaArReturn());
+
+            int a = SaskaitisanaArReturn();
+            Console.WriteLine(a);*/
+
+            int a = 5;
+            int b = 6;
+            Console.WriteLine(ArReturnUnParam(a, b));
+            Console.WriteLine(a + ", " + b);
+            //Console.WriteLine(ArReturnUnParam(1,2));
+            //Console.WriteLine(ArReturnUnParam(12, 78));
+
+        }
+
+        static void IfPiemers1()
+        {
             //parbaudit vai skaitlis ir lielaks par 0 un izvadit pazinojumu
             Console.WriteLine("Ievadi skaitli!");
             int skaitlis = Convert.ToInt32(Console.ReadLine());
             // > < => <= != ==
-            if (skaitlis > 0) 
+            if (skaitlis > 0)
             {
-                Console.WriteLine("Ir lielaks");    
+                Console.WriteLine("Ir lielaks");
             }
             else if (skaitlis == 0)
             {
@@ -22,13 +38,46 @@ namespace MyApp // Note: actual namespace depends on the project name.
             {
                 Console.WriteLine("Nav lielaks");
             }
+        }
 
+        static void IfPiemers2()
+        {
             bool a = false;
             a = !a;
 
             int b = 5;
             b = b + 6; //b vertiba ir 11
+
+            String simboli = "abc";
+            if (simboli == "aaa")
+            {
+                Console.Write("Simbolu virknes ir vienadas");
+            }
+
+            IfPiemers1();
         }
 
+        static int SaskaitisanaArReturn()
+        {
+            int a = 1;
+            int b = 2;
+
+            int result = a + b;
+            
+            return result;
+        }
+
+
+        static int ArReturnUnParam(int a, int b)
+        {
+            int result = a + b;
+
+            a = 55;
+            b = 7;
+
+            Console.WriteLine(a + ", " + b);
+
+            return result;
+        }
     }
 }
