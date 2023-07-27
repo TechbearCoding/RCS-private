d251442f1fc1d4f58c26ba95e9e279be12baeaf7
m4 <martins@techbearcoding.lv>
2023-07-27 17:34:37 +0300

    If samples

---
+++ day2_if/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //parbaudit vai skaitlis ir lielaks par 0 un izvadit pazinojumu
+            Console.WriteLine("Ievadi skaitli!");
+            int skaitlis = Convert.ToInt32(Console.ReadLine());
+            // > < => <= != ==
+            if (skaitlis > 0) 
+            {
+                Console.WriteLine("Ir lielaks");    
+            }
+            else if (skaitlis == 0)
+            {
+                Console.WriteLine("Skaitlis ir 0");
+            }
+            else
+            {
+                Console.WriteLine("Nav lielaks");
+            }
+
+            bool a = false;
+            a = !a;
+
+            int b = 5;
+            b = b + 6; //b vertiba ir 11
+        }
+
+    }
+}
