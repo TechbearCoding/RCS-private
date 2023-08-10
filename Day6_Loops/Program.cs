74c54442b086132e3ae3292bd6335338540af6ba
m4 <martins@techbearcoding.lv>
2023-08-10 14:02:08 +0300

    more samples

--- Day6_Loops/Program.cs
+++ Day6_Loops/Program.cs
@@ -4,7 +4,78 @@
     {
         static void Main(string[] args)
         {
-            Sample3();
+            int sk = Sample7();
+        }
+
+        private static int Sample7()
+        {
+            Boolean exit = false;
+            int skaitlis = 0;
+            while (exit == false)
+            {    
+                try
+                {
+                    Console.WriteLine("Ievadiet skaitli!");
+                    skaitlis = Convert.ToInt32(Console.ReadLine());
+                    exit = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Kluda, megini vel!");
+                }
+            }
+            return skaitlis;
+        }
+
+        private static void Sample6()
+        {
+            int skaitlis = 0;
+            try
+            {
+                Console.WriteLine("Ievadiet skaitli!");
+                skaitlis = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Kluda!");
+            }
+
+            Console.WriteLine(skaitlis);
+        }
+
+        private static void Sample5()
+        {
+            int a = 99;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Ievadiet skaitli!");
+                int skaitlis = Convert.ToInt32(Console.ReadLine());
+
+                if (skaitlis == a)
+                {
+                    Console.WriteLine("Uzmineji!");
+                    break;
+                }
+
+                Console.WriteLine("Neuzmineji!");
+            }
+        }
+
+        private static void Sample4()
+        {
+            while (true)
+            {
+                Console.WriteLine("Ievadiet virkni!");
+                String input = Console.ReadLine();
+                Console.WriteLine(input);
+
+                if(input == "exit")
+                {
+                    break;
+                }
+            }
+
         }
 
         static void Sample3()
