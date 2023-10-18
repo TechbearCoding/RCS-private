797c7cbee1f195bcff771db85120909d43121d74
Techbear <21012644+TechbearCoding@users.noreply.github.com>
2023-10-18 20:48:49 +0300

    day11

---
+++ Day11_objects/PrivateSample.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11_objects
+{
+    public class PrivateSample
+    {
+        public static int a = 1;
+        private static int b = 2;
+        public static void Run()
+        {
+            int a = 3;
+            b = 4;
+           
+            Console.WriteLine("Hello from PrivateSample");
+
+            RunPublic(a);
+            RunPublic(b);
+            RunPublic(PrivateSample.a);
+            RunPrivate();
+        }
+
+        private static void RunPrivate()
+        {
+            RunPublic(a);
+            Console.WriteLine("Hello from PrivateSample");
+        }
+
+        public static void RunPublic(int param)
+        {
+            Console.WriteLine(param);
+        }
+    }
+}
---
+++ Day11_objects/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Day11_objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrivateSample.Run();
+            Console.WriteLine(PrivateSample.a);
+
+            Students.vards= "Janis";
+            Students.uzvards = "Berzins";
+            Students.PievienotAtzimi(8);
+            Students.PievienotAtzimi(10);
+            Students.PievienotAtzimi(4);
+
+            Students.IzvaditInfo();
+        }
+
+    }
+}
