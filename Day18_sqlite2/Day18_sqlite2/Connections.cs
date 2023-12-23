1380dcb3ee7f4e10f7b2e9e48d779b85f262d0a9
Martins Leitarts <21012644+TechbearCoding@users.noreply.github.com>
2023-12-23 15:02:20 +0200

    task sample

---
+++ Day18_sqlite2/Day18_sqlite2/Connections.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+
+namespace Day18_sqlite2
+{
+    public class Connections
+    {
+        public static SQLiteConnection CreateConn()
+        {
+            String desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            String dbPath = "chinook.db";
+            String fullName = Path.Combine(desktopPath, dbPath);
+
+            SQLiteConnection sqlite_conn = new SQLiteConnection("Data Source=" + fullName + "; Version=3");
+
+            try
+            {
+                sqlite_conn.Open();
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+            }
+
+            return sqlite_conn;
+        }
+
+        public static void ReadCustomerCount(SQLiteConnection conn)
+        {
+            SQLiteDataReader sQLiteDataReader;
+            SQLiteCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT COUNT(customers.CustomerId) FROM customers";
+
+            sQLiteDataReader = cmd.ExecuteReader();
+
+            while (sQLiteDataReader.Read())
+            {
+                int line = sQLiteDataReader.GetInt32(0);
+                Console.WriteLine(line);
+            }
+        }
+
+        public static void ReadEmployeeInfo(SQLiteConnection conn)
+        {
+            SQLiteDataReader sQLiteDataReader;
+            SQLiteCommand cmd = conn.CreateCommand();
+            cmd.CommandText =
+                "SELECT(employees.FirstName || ' ' || employees.LastName) as 'Employee name', employees.Title, employees.City,employees.Country " +
+                "FROM employees;";
+
+            sQLiteDataReader = cmd.ExecuteReader();
+
+            while (sQLiteDataReader.Read())
+            {
+                String employeeName = sQLiteDataReader.GetString(0);
+                String employeeInfo = sQLiteDataReader.GetString(1);
+                String city = sQLiteDataReader.GetString(2);
+                String country = sQLiteDataReader.GetString(3);
+                
+
+                Console.WriteLine(employeeName + ": " + employeeInfo);
+                Console.WriteLine(country + ", " + city);
+                Console.WriteLine("------------");
+            }
+        }
+
+        public static void ReadMaxTrack(SQLiteConnection conn)
+        {
+            SQLiteDataReader sQLiteDataReader;
+            SQLiteCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT tracks.Name, MAX(tracks.Milliseconds)\r\nFROM tracks;";
+
+            sQLiteDataReader = cmd.ExecuteReader();
+
+            while (sQLiteDataReader.Read())
+            {
+                String name = sQLiteDataReader.GetString(0);
+                int line = sQLiteDataReader.GetInt32(1);
+
+                Console.WriteLine(name + ": " + line);
+            }
+        }
+
+        public static void ReadCustomerTracks(SQLiteConnection conn)
+        {
+            SQLiteDataReader sQLiteDataReader;
+            SQLiteCommand cmd = conn.CreateCommand();
+            cmd.CommandText =
+                "SELECT (customers.firstName || ' ' || customers.LastName) as 'Client name',\r\ntracks.Name\r\n" +
+                "FROM customers\r\nINNER JOIN invoices\r\n" +
+                "ON customers.CustomerId = invoices.CustomerId\r\nINNER JOIN invoice_items\r\n" +
+                "ON invoices.InvoiceId = invoice_items.InvoiceId\r\n" +
+                "INNER JOIN tracks\r\nON tracks.TrackId = invoice_items.TrackId;";
+
+            sQLiteDataReader = cmd.ExecuteReader();
+
+            while (sQLiteDataReader.Read())
+            {
+                String customerName = sQLiteDataReader.GetString(0);
+                String songName = sQLiteDataReader.GetString(1);
+
+                Console.WriteLine(customerName + ": " + songName);
+            }
+        }
+    }
+}
--- Day18_sqlite2/Day18_sqlite2/Program.cs
+++ Day18_sqlite2/Day18_sqlite2/Program.cs
@@ -6,7 +6,8 @@ namespace Day18_sqlite2
     {
         static void Main(string[] args)
         {
-            Izveleties();
+            SQLiteConnection conn = Connections.CreateConn();
+            Connections.ReadEmployeeInfo(conn);
         }
 
         static void Izveleties()
@@ -17,96 +18,24 @@ namespace Day18_sqlite2
             Console.WriteLine("3- klientu vardu un uzvardu un dziesmas kuras vini ir nopirkusi. ");
             int izvele = Convert.ToInt32(Console.ReadLine());
 
-            SQLiteConnection conn = CreateConn();
+            SQLiteConnection conn = Connections.CreateConn();
 
             if (izvele == 1)
             {
-                ReadCustomerCount(conn);
+                Connections.ReadCustomerCount(conn);
             }
             else if (izvele == 2)
             {
-                ReadMaxTrack(conn);
+                Connections.ReadMaxTrack(conn);
             }
             else if (izvele == 3)
             {
-                ReadCustomerTracks(conn);
+                Connections.ReadCustomerTracks(conn);
             }
 
             conn.Close();
         }
 
-        private static SQLiteConnection CreateConn()
-        {
-            String desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            String dbPath = "chinook.db";
-            String fullName = Path.Combine(desktopPath, dbPath);
-
-            SQLiteConnection sqlite_conn = new SQLiteConnection("Data Source=" + fullName + "; Version=3");
-
-            try
-            {
-                sqlite_conn.Open();
-            }
-            catch
-            {
-                Console.WriteLine("Error");
-            }
-
-            return sqlite_conn;
-        }
-
-        private static void ReadCustomerCount(SQLiteConnection conn)
-        {
-            SQLiteDataReader sQLiteDataReader;
-            SQLiteCommand cmd = conn.CreateCommand();
-            cmd.CommandText = "SELECT COUNT(customers.CustomerId) FROM customers";
-
-            sQLiteDataReader = cmd.ExecuteReader();
-
-            while (sQLiteDataReader.Read())
-            {
-                int line = sQLiteDataReader.GetInt32(0);
-                Console.WriteLine(line);
-            }
-        }
-
-        private static void ReadMaxTrack(SQLiteConnection conn)
-        {
-            SQLiteDataReader sQLiteDataReader;
-            SQLiteCommand cmd = conn.CreateCommand();
-            cmd.CommandText = "SELECT tracks.Name, MAX(tracks.Milliseconds)\r\nFROM tracks;";
-
-            sQLiteDataReader = cmd.ExecuteReader();
-
-            while (sQLiteDataReader.Read())
-            {
-                String name = sQLiteDataReader.GetString(0);
-                int line = sQLiteDataReader.GetInt32(1);
-
-                Console.WriteLine(name + ": " + line);
-            }
-        }
-
-        private static void ReadCustomerTracks(SQLiteConnection conn)
-        {
-            SQLiteDataReader sQLiteDataReader;
-            SQLiteCommand cmd = conn.CreateCommand();
-            cmd.CommandText =
-                "SELECT (customers.firstName || ' ' || customers.LastName) as 'Client name',\r\ntracks.Name\r\n" +
-                "FROM customers\r\nINNER JOIN invoices\r\n" +
-                "ON customers.CustomerId = invoices.CustomerId\r\nINNER JOIN invoice_items\r\n" +
-                "ON invoices.InvoiceId = invoice_items.InvoiceId\r\n" +
-                "INNER JOIN tracks\r\nON tracks.TrackId = invoice_items.TrackId;";
-
-            sQLiteDataReader = cmd.ExecuteReader();
-
-            while (sQLiteDataReader.Read())
-            {
-                String customerName = sQLiteDataReader.GetString(0);
-                String songName = sQLiteDataReader.GetString(1);
-
-                Console.WriteLine(customerName + ": " + songName);
-            }
-        }
+       
     }
 }
