7763bbfe0fbb4ef1331e40cadf5724c40f749734
Martins Leitarts <martins.leitarts@kleintech.net>
2024-04-06 14:48:37 +0300

    added smaple

---
+++ Day24_rest/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Day24_rest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebService webService = new WebService();
+            var client = new HttpClient();
+            webService.GetPosts(client);
+            webService.GetComment(client);
+        }
+    }
+}
---
+++ Day24_rest/WebService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day24_rest
+{
+    public class WebService
+    {
+        public void GetPosts(HttpClient client)
+        {
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, "https://jsonplaceholder.typicode.com/posts");
+            requestMessage.Headers.Add("Accept", "application/json");
+
+
+            var response = client.Send(requestMessage);
+
+            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+        }
+        public void GetComment(HttpClient client)
+        {
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, "https://jsonplaceholder.typicode.com/comments");
+            requestMessage.Headers.Add("Accept", "application/json");
+
+            var response = client.Send(requestMessage);
+
+            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+        }
+    }
+}
